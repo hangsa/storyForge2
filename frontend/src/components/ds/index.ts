82ef51ee95d3295a51a514757b04bda644b2b439
hangsa <lihailong0510@163.com>
2026-08-27 15:52:19 +0800

    feat(ds): export all 12 primitives + stage constants from barrel

--- frontend/src/components/ds/index.ts
+++ frontend/src/components/ds/index.ts
@@ -1,6 +1,41 @@
 // Nebula Forge design-system primitives.
-// Re-exported here so consumers can `import { BrandHeader } from "../components/ds"`
+// Re-exported here so consumers can `import { BrandHeader, PrimaryButton } from "../components/ds"`
 // instead of reaching into individual files.
 
 export { default as BrandHeader } from "./BrandHeader";
 export type { BrandHeaderProps } from "./BrandHeader";
+
+export { default as PrimaryButton } from "./PrimaryButton";
+export type { PrimaryButtonProps } from "./PrimaryButton";
+
+export { default as SecondaryButton } from "./SecondaryButton";
+export type { SecondaryButtonProps } from "./SecondaryButton";
+
+export { default as GhostButton } from "./GhostButton";
+export type { GhostButtonProps } from "./GhostButton";
+
+export { default as SearchInput } from "./SearchInput";
+export type { SearchInputProps } from "./SearchInput";
+
+export { default as DropdownSelect } from "./DropdownSelect";
+export type { DropdownSelectProps } from "./DropdownSelect";
+
+export { default as StatCard } from "./StatCard";
+export type { StatCardProps } from "./StatCard";
+
+export { default as PanelCard } from "./PanelCard";
+export type { PanelCardProps } from "./PanelCard";
+
+export { default as PhaseIndicator } from "./PhaseIndicator";
+export type { PhaseIndicatorProps, PhaseIndicatorPhase } from "./PhaseIndicator";
+
+export { default as ProjectTableRow } from "./ProjectTableRow";
+export type { ProjectTableRowProps } from "./ProjectTableRow";
+
+export { default as Sidebar } from "./Sidebar";
+export type { SidebarProps } from "./Sidebar";
+
+export { default as SidebarNavItem } from "./SidebarNavItem";
+export type { SidebarNavItemProps } from "./SidebarNavItem";
+
+export { STAGE_COLORS, STAGE_LABELS, isPreWizardStage } from "./stages";
